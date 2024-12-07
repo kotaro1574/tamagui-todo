17e3b15531d546ba234cb5a28385f5d5bc1a65b9
kotaro1574 <shake1574@gmail.com>
2024-12-07 11:44:48 +0900

    update: todo機能

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,28 @@
-import { Text } from "react-native";
+import { useState } from "react";
+import { View, Text, H1, YStack, Input, Button } from "tamagui";
 
 export default function Page() {
-  return <Text>Home page</Text>;
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState<string[]>([]);
+
+  const handleSubmit = () => {
+    if (!inputValue) return;
+    setTodos([...todos, inputValue]);
+    setInputValue("");
+  };
+
+  return (
+    <View margin={10}>
+      <YStack gap={"$-1"}>
+        <H1 fontSize={"$8"}>TODO作成</H1>
+        <Input value={inputValue} onChangeText={setInputValue} />
+        <Button onPress={handleSubmit}>追加</Button>
+        <YStack gap={"$-1"}>
+          {todos.map((todo, index) => (
+            <Text key={index}>{todo}</Text>
+          ))}
+        </YStack>
+      </YStack>
+    </View>
+  );
 }
